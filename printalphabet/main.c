c220623e20fca51eaba37f7970021547481c629d
victorpaularony <smallypauls@gmail.com>
2024-06-13 15:30:12 +0300

    FILE: main.go file

---
+++ printalphabet/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char i = 'a';
+    for(i = 'a'; i<= 'z';i++){
+        printf("%c", i);
+    }
+    printf("\n");
+    for (i='A';i<= 'Z';i++){
+        printf("%c", i);
+    }
+    printf("\n");
+}
